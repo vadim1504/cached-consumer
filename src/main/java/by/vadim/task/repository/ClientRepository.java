28a16a6722a30b78d57b222936a216d9e0a60926
vadim1504 <vadi-96@mail.ri>
2018-02-18 15:35:05 +0300

    i

--- src/main/java/by/vadim/task/repository/ClientRepository.java
+++ src/main/java/by/vadim/task/repository/ClientRepository.java
@@ -1,10 +1,12 @@
 package by.vadim.task.repository;
 
 import by.vadim.task.model.Client;
+import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+@N1qlPrimaryIndexed
 public interface ClientRepository extends CrudRepository<Client, Integer> {
 
     List<Client> findByBalanceValueBefore(int value);
