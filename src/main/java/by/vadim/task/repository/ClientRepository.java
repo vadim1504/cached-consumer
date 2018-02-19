670b73a908107e7e3f4d004bcbb7062dee856f41
Vadim Shestov <vadi-96@mail.ru>
2018-02-19 12:22:35 +0300

    Update ClientRepository.java

--- src/main/java/by/vadim/task/repository/ClientRepository.java
+++ src/main/java/by/vadim/task/repository/ClientRepository.java
@@ -1,12 +1,12 @@
 package by.vadim.task.repository;
 
 import by.vadim.task.model.Client;
-import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
+import org.springframework.data.couchbase.core.query.N1qlSecondaryIndexed;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-@N1qlPrimaryIndexed
+@N1qlSecondaryIndexed(indexName = "idx_Balance_Value")
 public interface ClientRepository extends CrudRepository<Client, Integer> {
 
     List<Client> findByBalanceValueBefore(int value);
