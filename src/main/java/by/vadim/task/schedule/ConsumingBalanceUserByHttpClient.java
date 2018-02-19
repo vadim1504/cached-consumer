7f512f4821bfc1bbdd5e55bcfbe5750e4450e539
Vadim Shestov <vadi-96@mail.ru>
2018-02-19 13:48:02 +0300

    Update ConsumingBalanceUserByHttpClient.java

--- src/main/java/by/vadim/task/schedule/ConsumingBalanceUserByHttpClient.java
+++ src/main/java/by/vadim/task/schedule/ConsumingBalanceUserByHttpClient.java
@@ -44,7 +44,7 @@ public class ConsumingBalanceUserByHttpClient {
     public void getResponse() throws IOException, SAXException, TransformerException, JAXBException {
         ResponseEntity<String> response = null;
         try {
-            response = restTemplate.exchange("http://http://192.168.99.100:8080/tariff-store/user", HttpMethod.GET, entity, String.class);
+            response = restTemplate.exchange("http://192.168.99.100:8080/tariff-store/user", HttpMethod.GET, entity, String.class);
         }catch (Exception exception){
             System.out.println("Application #1 is dead");
         }
